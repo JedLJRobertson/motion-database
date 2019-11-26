ad511a375153e3a7711858fbaad573362ac2dfc1
Jed Robertson <jrob928@aucklanduni.ac.nz>
2019-11-26 14:44:26 +1300

    Added ID's to infoslides on motion API

--- MotionDatabase/MotionDatabase/Dto/InfoSlideDto.cs
+++ MotionDatabase/MotionDatabase/Dto/InfoSlideDto.cs
@@ -5,10 +5,12 @@ namespace MotionDatabaseBackend.Dto
 {
     public class InfoSlideDto
     {
+        public int Id { get; set; }
         public string Text { get; set; }
 
         public InfoSlideDto(MotionInfoSlide infoSlide)
         {
+            Id = infoSlide.Id;
             Text = infoSlide.InfoSlideText;
         }
     }
