dfb583e3da9215e689353bdd4234441800bd1559
Jed Robertson <jrob928@aucklanduni.ac.nz>
2019-11-26 14:41:56 +1300

    Added info slides to domain

---
+++ MotionDatabase/MotionDatabase/Dto/InfoSlideDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MotionDatabaseBackend.Models;
+
+namespace MotionDatabaseBackend.Dto
+{
+    public class InfoSlideDto
+    {
+        public string Text { get; set; }
+
+        public InfoSlideDto(MotionInfoSlide infoSlide)
+        {
+            Text = infoSlide.InfoSlideText;
+        }
+    }
+}
